35802a76ba68f1a33bd5b014745ae61dafabdec6
hlynur92 <43953815+hlynur92@users.noreply.github.com>
2023-03-06 16:51:53 +0100

    Add project files.

---
+++ DatabaseCompulsory/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+
+string connectionString =
+    @"Data Source=DESKTOP-3BNJJUV\MSSQLSERVER01;" + //Server Name
+    "Initial Catalog=Company;" + //Database
+    @"User id=DESKTOP-3BNJJUV\Hlynur;" + //Username
+    "Password="; //Password
+
+/*
+ 
+ */
+
+var connection = new SqlConnection(connectionString);
+connection.Open();
+
+//Do Stuff
+
+connection.Close();
